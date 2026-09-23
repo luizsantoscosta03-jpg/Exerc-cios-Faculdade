8f2afd624f132f0d91c893d23f923d15e35ddc1c
Luiz <luizsantoscosta03@gmail.com>
2026-09-23 00:12:38 -0300

    Adicionar exercicio 3 - while

---
+++ src/EXERCICIO3WHILE.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class EXERCICIO3WHILE {
+} public static void main(String[] args)
+{ Scanner leitor = new Scanner(System.in);
+    int numero = leitor.nextInt();
+    while (numero != 0) {
+        numero = leitor.nextInt();
+    }
+System.out.println("Programa Encerrado");
+
+
+}
